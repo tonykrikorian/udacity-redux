fbe49d40580a96bc52e3a281a6eba39455eb5d10
Tony <tony.krikorian@outlook.com>
2020-03-03 21:36:16 -0300

    Subscribe funcionality

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 function createStore() {
   let state;
+  let listeners = [];
 
   const getState = () => state;
+  const subscribe = listener => {
+    listeners.push(listener);
+    return () => {
+      listeners = listeners.filter(l => l !== listener);
+    };
+  };
 
   return {
-    getState
+    getState,
+    subscribe
   };
 }
+
+const store = createStore();
+store.subscribe(() => {
+  console.log("The new state is", store.getState());
+});
