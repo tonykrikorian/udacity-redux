43b8c2291b1b229d95484a30ecd42ce2f8eb441f
Tony <tkrikoriam@achs.cl>
2020-03-04 16:36:45 -0300

    Refactor strings with onstants

--- index.js
+++ index.js
@@ -24,14 +24,20 @@ function createStore(reducer) {
   };
 }
 
+const ADD_TODO = "ADD_TODO";
+const REMOVE_TODO = "REMOVE_TODO";
+const TOGGLE_TODO = "TOGGLE_TODO";
+const ADD_GOAL = "ADD_GOAL";
+const REMOVE_GOAL = "REMOVE_GOAL";
+
 //App Code
 function todos(state = [], action) {
   switch (action.type) {
-    case "ADD_TODO":
+    case ADD_TODO:
       return state.concat([action.todo]);
-    case "REMOVE_TODO":
+    case REMOVE_TODO:
       return state.filter(todo => todo.id !== action.id);
-    case "TOGGLE_TODO":
+    case TOGGLE_TODO:
       return state.map(todo =>
         todo.id !== action.id
           ? todo
@@ -43,13 +49,95 @@ function todos(state = [], action) {
   }
 }
 
-const store = createStore(todos);
+function goals(state = [], action) {
+  switch (action.type) {
+    case ADD_GOAL:
+      return state.concat([action.goal]);
+    case REMOVE_GOAL:
+      return state.filter(goal => goal.id !== action.id);
+    default:
+      return state;
+  }
+}
+
+function app(state = {}, action) {
+  return {
+    todos: todos(state.todos, action),
+    goals: goals(state.goals, action)
+  };
+}
+const store = createStore(app);
 store.subscribe(() => console.log("This is the new state", store.getState()));
+
 store.dispatch({
-  type: "ADD_TODO",
+  type: ADD_TODO,
   todo: {
     id: 0,
     name: "Learn redux",
     complete: false
   }
+});
+
+store.dispatch({
+  type: ADD_TODO,
+  todo: {
+    id: 1,
+    name: "Learn React",
+    complete: false
+  }
+});
+
+store.dispatch({
+  type: ADD_TODO,
+  todo: {
+    id: 2,
+    name: "Learn Linux",
+    complete: false
+  }
+});
+
+store.dispatch({
+  type: REMOVE_TODO,
+  id: 2
+});
+
+store.dispatch({
+  type: REMOVE_TODO,
+  id: 1
+});
+
+store.dispatch({
+  type: REMOVE_TODO,
+  id: 0
+});
+
+store.dispatch({
+  type: TOGGLE_TODO,
+  id: 0
+});
+
+store.dispatch({
+  type: TOGGLE_TODO,
+  id: 1
+});
+
+store.dispatch({
+  type: ADD_GOAL,
+  goal: {
+    id: 0,
+    name: "Learn Desaign Patterns"
+  }
+});
+
+store.dispatch({
+  type: ADD_GOAL,
+  goal: {
+    id: 1,
+    name: "Learn SQL"
+  }
+});
+
+store.dispatch({
+  type: "REMOVE_GOAL",
+  id: 0
 });
