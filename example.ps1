17c2d79ec22ce8bee2aaf4ce067ccf535651a464
Tony <tkrikoriam@achs.cl>
2020-03-09 08:05:48 -0300

    Add optimistic update

--- example.ps1
+++ example.ps1
@@ -3,19 +3,29 @@
 
 #Verifica sí el proceso ServerMultibrowser4 se encuentra en ejecución
 $multiBrowserObject = Get-Process -Name "ServerMultibrowser4" -ErrorAction SilentlyContinue
+$fpSensorObject = Get-Process -Name "FPSensor" -ErrorAction SilentlyContinue
 
 
-#Sí el proceso no esta iniciado,entonces inicia proceso ServerMultibrowser4,sino , lo reinicia
-if (!$multiBrowserObject) {
+
+
+# #Sí el proceso no esta iniciado,entonces inicia proceso ServerMultibrowser4,sino , lo reinicia
+if (!$multiBrowserObject -or !$fpSensorObject) {
     $multiBrowserObject = Start-Process -FilePath "ServerMultibrowser4.exe" -WorkingDirectory "C:\autentia\Multibrowser4"
+    Start-Sleep -s 3
+    $fpSensorObject = Start-Process -FilePath "FPSensor.exe" -WorkingDirectory "C:\autentia\bin"
     Write-Host $multiBrowserObject.id
 }
-elseif ($multiBrowserObject) {
-    
-    Stop-Process -Id $multiBrowserObject.id
-    Start-Sleep -s 1
-    $multiBrowserObject = Start-Process -FilePath "ServerMultibrowser4.exe" -WorkingDirectory "C:\autentia\Multibrowser4"
-    Write-Host $multiBrowserObject.id
-}
+# elseif ($multiBrowserObject -and $fpSensorObject) {
+#     $multiBrowserObject = Get-Process -Name "ServerMultibrowser4" -ErrorAction SilentlyContinue
+#     $fpSensorObject = Get-Process -Name "FPSensor" -ErrorAction SilentlyContinue
+#     Stop-Process -Id $multiBrowserObject.id
+#     Start-Sleep -s 2
+#     Stop-Process -Id $fpSensorObject.id
+#     Start-Sleep -s 2
+#     $multiBrowserObject = Start-Process -FilePath "ServerMultibrowser4.exe" -WorkingDirectory "C:\autentia\Multibrowser4"
+#     Start-Sleep -s 2
+#     $fpSensorObject = Start-Process -FilePath "FPSensor.exe" -WorkingDirectory "C:\autentia\bin"
+#     Write-Host $multiBrowserObject.id
+# }
 
 
